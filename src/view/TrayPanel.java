53ab9d34ef8f87efb1433ce381b61329107668b7
Sebastian <witonsebastian90@gmail.com>
2025-05-25 07:22:34 -1200

    GUI v3

--- src/view/TrayPanel.java
+++ src/view/TrayPanel.java
@@ -41,14 +41,62 @@ public class TrayPanel extends JPanel {
 
     // Genera un piatto in modo casuale in circa holes.length/3 buchi
     public void ensureInitialPlates() {
-        int count = holes.length/3;
-        List<Integer> idxs = new ArrayList<>();
-        for (int i = 0; i < holes.length; i++) idxs.add(i);
-        Collections.shuffle(idxs);
-        for (int i = 0; i < count; i++) {
-            holes[idxs.get(i)].generatePlate();
+        int count = holes.length / 3; // numero massimo di piatti da generare
+        List<Integer> availableIndexes = new ArrayList<>();
+
+        // Popola gli indici disponibili, evitando i bordi per semplificare il controllo
+        for (int i = 0; i < holes.length; i++) {
+            int r = i / cols;
+            int c = i % cols;
+
+            if (r > 0 && r < rows - 1 && c > 0 && c < cols - 1) {
+                availableIndexes.add(i);
+            }
         }
+
+        Collections.shuffle(availableIndexes);
+        Set<Integer> used = new HashSet<>();
+
+        int placed = 0;
+        for (int i : availableIndexes) {
+            if (placed >= count) break;
+
+            if (isIsolated(i, used)) {
+                PlateComponent hole = holes[i];
+                do {
+                    hole.generatePlate();
+                } while (hole.getModel().getPieces().size() == 6);
+                used.add(i);
+                placed++;
+            }
+        }
+
+        revalidate();
+        repaint();
     }
+
+    private boolean isIsolated(int index, Set<Integer> used) {
+        int r = index / cols;
+        int c = index % cols;
+
+        // Controlla le celle adiacenti (compreso diagonali)
+        for (int dr = -1; dr <= 1; dr++) {
+            for (int dc = -1; dc <= 1; dc++) {
+                int nr = r + dr;
+                int nc = c + dc;
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
+                    int neighborIndex = nr * cols + nc;
+                    if (used.contains(neighborIndex)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+
 
     // Trova il buco sotto il punto di drop
     public PlateComponent getHoleAtPoint(Point point) {
